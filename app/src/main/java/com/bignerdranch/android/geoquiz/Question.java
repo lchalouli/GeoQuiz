03683d48111aca1e2fffd0a967084656f31715aa
Chalouli <Lela>
2017-07-13 17:21:31 +0200

    First commit

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,31 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by lchalouli on 12/07/2017.
+ */
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int mTextResId, boolean mAnswerTrue) {
+        this.mTextResId = mTextResId;
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
